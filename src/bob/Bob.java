1ea0a846c389cf8d0cb1eaff67d7fd5d739a1800
dante.arab <dante.arab@1H85172FHT.learnet.se>
2026-05-11 16:13:41 +0200

    jag har ändrat hur mina händelse block ser ut och fungerar genom att skriva frågor

--- src/bob/Bob.java
+++ src/bob/Bob.java
@@ -6,7 +6,8 @@ public class Bob {
  
   
     public static void main(String[] args) {
-        
+        String svara;
+        int svar;
         int tur = 0;
         Scanner input= new Scanner(System.in);
         System.out.println("hur många spelare är ni ");
@@ -19,7 +20,7 @@ public class Bob {
             namn[i] = input.nextLine();
         } 
         
-        for (int i = 0; i < 400; i++) {
+        for (int i = 0; i < 1000; i++) {
             System.out.println("det är spelare " + namn[tur]);
             int  tärning = (int)(Math.random()*6 + 1);
             System.out.println("du slog en " +tärning);
@@ -30,27 +31,89 @@ public class Bob {
                 System.out.println("du är på plats " + plats[tur]);
                 
                
-                //händelse block som låter användare få lite roligt 
+                //händelse block som låter användare ha lite roligt 
                 if (plats[tur] == 2) {
-                    System.out.println("här händer +2");
-                    plats[tur]+=2;
+                    System.out.println("här händer +2 om du får rätt");
+                    System.out.println("vad är 2 + 2");
+                    svar = input.nextInt();
+                    if (svar==4) {
+             
+                       plats[tur]+=2;  
+                        System.out.println("du fick rätt");
+                    }
+                    else{
+                         plats[tur]--; 
+                        System.out.println("du fick fel");
+                        System.out.println("och gick bak ett steg");
+                    }
                    
                     System.out.println("du är på plats " + plats[tur]);
                    
                 }
+                // den slumpar tre tärningar som ska bli större än 15 eller mindre än 10
                 else if (plats[tur] == 77) {
-                    System.out.println("här är det -7");
-                    plats[tur]-=7; 
+                    System.out.println("här är det -7 om du fått otur ");
+                    int a  = (int)(Math.random()*6 + 1);
+                    int b  = (int)(Math.random()*6 + 1);
+                    int c  = (int)(Math.random()*6 + 1);
+                      System.out.println("summan blev " + a+b+c);
+                    if (a+b+c >=15) {
+                        plats[tur]+=7; 
+                        System.out.println("du fick tur");
+                        
+                    }
+                    else if(a+b+c <= 10)  {
+                        plats[tur]-=7; 
+                        System.out.println("du fick otur");
+                    }
+                  
                     System.out.println("du är på plats " + plats[tur]);  
                 }
+                // den låter spelarna svara på en fråga om jorden 
                 else if (plats[tur] == 7) {
-                    System.out.println("här är det -1");
-                    plats[tur]--;
+                    System.out.println("här är det -2 om du får fel på denna fråga");
+                    System.out.println("vad är det största landet");
+                    svara = input.nextLine();
+                    if (svara.equals("ryssland") ) {
+                        plats[tur]+=2;
+                        System.out.println("du fick rätt");
+                    }
+                    else{
+                    plats[tur]-=2;
+                        System.out.println("du fick fel");
+                    }
+                    
                     System.out.println("du är på plats " + plats[tur]); 
                 } 
+                //den kollar om man fått yatzy och om man inte fått då är det inte yatzy
                 else if (plats[tur] == 15) {
-                    System.out.println("här är det +1");
-                    plats[tur]++;
+                    System.out.println("här är det +1 om du får yatzy");
+                    boolean yatzy = false;
+                    for (int j = 0; j < 3; j++) {
+                    
+                    int a  = (int)(Math.random()*6 + 1);
+                    int b  = (int)(Math.random()*6 + 1);
+                    int c  = (int)(Math.random()*6 + 1);
+                    int d  = (int)(Math.random()*6 + 1);
+                    int e  = (int)(Math.random()*6 + 1);
+                    System.out.print(a);
+                    System.out.print(b);
+                    System.out.print(c);
+                    System.out.print(d);
+                    System.out.println(e);
+                    if (a==b && b==c && c==d && d==e ) {
+                        yatzy = true;
+                        plats[tur]++; 
+                        System.out.println("du fick yatzy +1");
+                        break;
+                    }
+                      
+                    }
+                    if (yatzy == false) {
+                         plats[tur]--; 
+                         System.out.println("du fick inte yatzy -1");
+                    }
+                  
                     System.out.println("du är på plats " + plats[tur]);
                 } 
                 else if (plats[tur] == 28) {
